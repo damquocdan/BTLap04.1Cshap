5b674870ca05feaba28ebf1bf092fa75b6d71d7b
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-08-03 11:30:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BTLap04._1Cshap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(1000);
+            // in ra số tiền trong tài khoản
+            Console.WriteLine("Số tiền trong tài khoản: "+ account.GetBalance);
+            // nạp thêm 500 vào tài khoản
+            account.Deposit(500);
+            // in số tiền trong tài khoản sau khi nạp
+            Console.WriteLine("Số tiền trong tài khoản sau khi nạp:" +account.GetBalance);
+            // Rút 300 từ tài khoản
+            account.WithDraw(300);
+            // In ra số tiền trong tài khoản sau khi rút
+            Console.WriteLine("Số tiền trong tài khoản sau khi rút:"+account.GetBalance);
+            Console.ReadKey();
+        }
+
+    }
+}
